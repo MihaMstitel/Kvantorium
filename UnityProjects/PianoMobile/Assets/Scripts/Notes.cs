4c7d2e32f90d382de37041fe87165c291b6151b2
MihaMstitel <ggbawlgem@gmail.com>
2024-05-14 20:05:34 +0900

    added my Final PROJECT :)

---
+++ UnityProjects/PianoMobile/Assets/Scripts/Notes.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    public AudioSource Note_C;
+    public AudioSource Note_D;
+    public AudioSource Note_E;
+    public AudioSource Note_F;
+    public AudioSource Note_G;
+    public AudioSource Note_A;
+    public AudioSource Note_B;
+    public AudioSource Note_C1;
+    public AudioSource Note_D1;
+    public AudioSource Note_F1;
+    public AudioSource Note_Cs;
+    public AudioSource Note_Ds;
+    public AudioSource Note_Fs;
+    public AudioSource Note_Gs;
+    public AudioSource Note_Bb;
+    public AudioSource Note_Cs1;
+    public AudioSource Note_Ds1;
+
+    public void PlayC()
+    {
+        Note_C.Play();
+    }
+        public void PlayD()
+    {
+        Note_D.Play();
+    }
+        public void PlayE()
+    {
+        Note_E.Play();
+    }
+        public void PlayF()
+    {
+        Note_F.Play();
+    }
+        public void PlayG()
+    {
+        Note_G.Play();
+    }
+        public void PlayA()
+    {
+        Note_A.Play();
+    }
+        public void PlayB()
+    {
+        Note_B.Play();
+    }
+        public void PlayC1()
+    {
+        Note_C1.Play();
+    }
+        public void PlayD1()
+    {
+        Note_D1.Play();
+    }
+        public void PlayF1()
+    {
+        Note_F1.Play();
+    }
+        public void PlayCs()
+    {
+        Note_Cs.Play();
+    }
+        public void PlayDs()
+    {
+        Note_Ds.Play();
+    }
+        public void PlayFs()
+    {
+        Note_Fs.Play();
+    }
+        public void PlayGs()
+    {
+        Note_Gs.Play();
+    }
+        public void PlayBb()
+    {
+        Note_Bb.Play();
+    }
+        public void PlayCs1()
+    {
+        Note_Cs1.Play();
+    }
+        public void PlayDs1()
+    {
+        Note_Ds1.Play();
+    }
+
+}
